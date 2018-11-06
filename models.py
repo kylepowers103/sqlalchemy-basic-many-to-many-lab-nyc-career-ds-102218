31c54cdcd80ec384030cd1f4aca7acf1bd049823
Kyle Powers <kyle.powers103@gmail.com>
2018-11-06 16:25:49 -0500

    adsf

--- models.py
+++ models.py
@@ -5,9 +5,26 @@ from sqlalchemy.orm import sessionmaker, relationship
 Base = declarative_base()
 
 # Write your classes below
+class Role(Base):
+    __tablename__ = "roles"
+    id = Column(Integer, primary_key=True)
+    character = Column(String, nullable=False)
+    actor_id = Column(Integer, ForeignKey('actors.id'))
+    actors = relationship('Actor', secondary='actor_roles')
+
+class Actor(Base):
+    __tablename__ = "actors"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    roles = relationship('Role', secondary='actor_roles')
+
+class ActorRole(Base):
+    __tablename__ = "actor_roles"
+    role_id= Column(Integer, ForeignKey('roles.id'),primary_key=True)
+    actor_id = Column(Integer, ForeignKey('actors.id'),primary_key=True)
 
 
 
 
-engine = create_engine('sqlite:///:memory:')
+engine = create_engine('sqlite:///movies.db')
 Base.metadata.create_all(engine)
--- queries.py
+++ queries.py
@@ -1,13 +1,23 @@
 from models import *
+from sqlalchemy.orm import sessionmaker
+
+session = sessionmaker()
+session.configure(bind=engine)
+Base.metadata.bind = engine
+
+session = session()
 
 def return_christian_bales_roles(session):
-    pass
+    return session.query(Actor).filter(Actor.name=="Christian Bale").first().roles
+    #return session.querey(Player)..join(Team).join(City).filter(City.name=="Los Angeles").all()
     # Return a list of Christian Bale role instances
 
-def return_catwoman_actors(session):
-    pass
-    #  Return a list of actor instances that have played Catwoman
 
+def return_catwoman_actors(session):
+    return session.query(Role).filter(Role.character=="Catwoman").first().actors
+
+    #  Return a list of actor instances that have played Catwoman
+    #catwoman.actors[0].name
 def return_number_of_batman_actors(session):
-    pass
+    return len(session.query(Role).filter(Role.character=="Batman").first().actors)
     # Return the number of actors that have played Batman
